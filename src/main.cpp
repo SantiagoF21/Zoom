315625b1aaa5569e66cdd42269f3bbb1be3f577a
Santiago Fernandez <santiagofdz18@gmail.com>
2026-09-02 08:47:27 -0400

    chore: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
+#include <cctype>
+
+/*
+    Zoom (.zm)
+*/
+
+enum class TokenType {
+    _return,
+    int_lit,
+    semi
+};
+
+struct Token {
+    TokenType token_type;
+    std::optional<std::string> value;
+};
+
+std::vector<Token> tokenize(const std::string& str) {
+    for (char c : str) {
+        std::cout << c << std::endl;
+    }
+
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
+        std::cerr << "zoom <input.zm>" << std::endl;
+        return 1;
+    }
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    tokenize(contents);
+
+    std::cout << contents << std::endl;
+    
+    std::cout << argv[1] << std::endl;
+    return 0;
+}
