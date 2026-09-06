8bc6a0d7a7f57eac64b0cf96a5a8504346a5a325
SantiagoF21 <santiagofdz18@gmail.com>
2026-09-06 16:47:54 -0400

    chore: renamed files and class from Tokenizer to Lexer

---
+++ src/lexer.cpp
@@ -0,0 +1,61 @@
+#include "lexer.hpp"
+
+#include <iostream>
+#include <sstream>
+
+Lexer::Lexer(const std::string& src_code) : m_src_code(std::move(src_code)) {}
+
+std::vector<Token> Lexer::tokenize() {
+    std::vector<Token> tokens;
+    std::string buffer;
+
+    while(auto character = peek()) {
+        if (CharInfo::is_alpha(*character)) {
+            buffer.push_back(consume());
+            auto next_alpha = peek();
+            while (next_alpha && CharInfo::is_alpha(*next_alpha)) {
+                buffer.push_back(consume());
+                next_alpha = peek();
+            }
+            if (buffer == "exit") {
+                tokens.push_back({.id = TokenID::_exit});
+                buffer.clear();
+            } else {
+                std::cerr << "Lexing Error: The string " + buffer + " could not be formatted into a token.";
+                exit(1);
+            }
+        } else if (CharInfo::is_digit(*character)) {
+            buffer.push_back(consume());
+            auto next_digit = peek();
+            while (next_digit && CharInfo::is_digit(*next_digit)) {
+                buffer.push_back(consume());
+                next_digit = peek();
+            }
+            tokens.push_back({.id = TokenID::_int_literal, .value = buffer});
+            buffer.clear();
+        } else if (CharInfo::is_semi(*character)) {
+            consume();
+            tokens.push_back({ .id = TokenID::_semi});
+        } else if (CharInfo::is_white_space(*character))  {
+            consume();
+            continue;
+        } else {
+            std::cerr << "Lexing Error: Unexpected character '" << *character << ".'";
+            exit(1);
+        }
+    }
+    m_index = 0;
+    return tokens;
+}
+
+std::optional<char> Lexer::peek(size_t index_offset) const {
+    if (m_index + index_offset >= m_src_code.length()) {
+        return {};
+    } else {
+        return m_src_code[m_index + index_offset];
+    }
+}
+
+char Lexer::consume() {
+    return m_src_code.at(m_index++);
+}
