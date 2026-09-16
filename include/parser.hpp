adf9b94267b1635968cd4036a928fd1a43d58f23
SantiagoF21 <santiagofdz18@gmail.com>
2026-09-16 17:41:57 -0400

    fix(parser): correct includes in parser.hpp

---
+++ include/parser.hpp
@@ -0,0 +1,30 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <vector>
+#include <optional>
+#include <string>
+
+#include "node.hpp"
+#include "token.hpp"
+
+class Parser {
+public:
+    explicit Parser(std::vector<Token> tokens);
+
+    std::vector<Node::Instruction> parse_program();
+private:
+    std::optional<Node::Term> parse_term();
+    std::optional<Node::Expression> parse_expr();
+    std::optional<Node::Instruction> parse_instr();
+
+    const std::vector<Token> m_tokens;
+    size_t m_index{};
+
+    std::optional<Token> peek(size_t index_offset = 0) const;
+    Token consume();
+
+    void expect(TokenID id, const std::string& err_msg);
+};
+
+#endif
